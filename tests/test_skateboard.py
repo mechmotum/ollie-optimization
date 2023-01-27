f7b45edd1d9cb1b3f2dc08f2f25b9b26314cbee5
Sam Brockie <sambrockie@icloud.com>
2023-01-27 15:22:45 +0100

    Add second test function

--- tests/test_skateboard.py
+++ tests/test_skateboard.py
@@ -7,11 +7,16 @@ from skateboard import SegmentSkateboard
 
 
 def test_skateboard_has_wheelbase_attribute():
-
     skateboard = SegmentSkateboard()
     assert hasattr(skateboard, 'wheelbase')
     assert skateboard.wheelbase == 0.2
 
 
-if __name__ == "__name__":
+def test_skateboard_is_segment_skateboard():
+    skateboard = SegmentSkateboard(wheelbase=0.3)
+    assert skateboard.wheelbase == 0.3
+
+
+if __name__ == "__main__":
     test_skateboard_has_wheelbase_attribute()
+    test_skateboard_is_segment_skateboard()
