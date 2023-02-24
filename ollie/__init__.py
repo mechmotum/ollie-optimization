d6fefcbbfde40ade3aa6789b4d3f3866eef310ad
Sam Brockie <sambrockie@icloud.com>
2023-02-24 20:16:35 +0100

    Move package from skateboard to ollie

---
+++ ollie/__init__.py
@@ -0,0 +1,28 @@
+""""""
+
+from ollie.human import ForceHuman
+from ollie.ollie import Ollie
+from ollie.material import Glue, Polyurethane, Maple, Steel
+from ollie.skateboard import (
+    Axle,
+    Wheel,
+    Truck,
+    FlatRectangularDeck,
+    SegmentDeck,
+    Skateboard,
+)
+
+__all__ = [
+    ForceHuman,
+    Ollie,
+    Glue,
+    Polyurethane,
+    Maple,
+    Steel,
+    Axle,
+    Wheel,
+    Truck,
+    FlatRectangularDeck,
+    SegmentDeck,
+    Skateboard,
+]
