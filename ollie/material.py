aca50bb60d46598f9d7e2f0805234562d09521dc
Sam Brockie <sambrockie@icloud.com>
2023-03-22 22:16:22 +0100

    Add ABC and subclass, add docstrings

--- ollie/material.py
+++ ollie/material.py
@@ -1,15 +1,21 @@
-""""""
+"""Materials and their properties."""
 
 import dataclasses
+from abc import ABC
 
 import sympy as sm
 
 from ollie.container import Container
 
 
+class Material(ABC):
+    """Abstract base class for material dataclasses."""
+    pass
+
+
 @dataclasses.dataclass
-class Glue:
-    """"""
+class Glue(Material):
+    """Dataclass container properties of glue."""
 
     specific_mass: Container = Container(
         symbol=sm.Symbol(r"\gamma_{glue}"), value=0.210
@@ -17,21 +23,21 @@ class Glue:
 
 
 @dataclasses.dataclass
-class Polyurethane:
-    """"""
+class Polyurethane(Material):
+    """Dataclass containing properties of polyurethane."""
 
     density: Container = Container(symbol=sm.Symbol(r"\rho_{pu}}"), value=1130)
 
 
 @dataclasses.dataclass
-class Maple:
-    """"""
+class Maple(Material):
+    """Dataclass containing properties of maple."""
 
     density: Container = Container(symbol=sm.Symbol(r"\rho_{maple}"), value=705.0)
 
 
 @dataclasses.dataclass
-class Steel:
-    """"""
+class Steel(Material):
+    """Dataclass containing properties of steel."""
 
     density: Container = Container(symbol=sm.Symbol(r"\rho_{steel}"), value=7700.0)
