25b455df5f815fff9f93e4d8f18ed63e2deeac9b
Sam Brockie <sambrockie@icloud.com>
2023-03-24 14:33:38 +0000

    Add inertia_of_cylinder function

--- ollie/inertia.py
+++ ollie/inertia.py
@@ -23,3 +23,29 @@ def inertia_of_cuboid(
         twelfth * mass * (z_dim**two + x_dim**two),
         twelfth * mass * (x_dim**two + y_dim**two),
     )
+
+
+def inertia_of_cylinder(
+    frame: me.ReferenceFrame,
+    mass: Container,
+    *,
+    axis: me.Vector,
+    radius: Container,
+    length: Container,
+) -> me.Dyadic:
+    """Calculate the inertia of a cylinder."""
+    if axis not in frame:
+        msg = f"Axis {axis} must be a unit vector of frame {frame}."
+        raise ValueError(msg)
+    two = sm.Integer(2)
+    three = sm.Integer(3)
+    half = sm.Rational(1, 2)
+    twelfth = sm.Rational(1, 12)
+    parallel = half * mass * radius**two
+    perpendicular = twelfth * mass * (height**two + three*radius**two)
+    return me.inertia(
+        frame,
+        parallel if axis is frame.x else perpendicular,
+        parallel if axis is frame.y else perpendicular,
+        parallel if axis is frame.z else perpendicular,
+    )
