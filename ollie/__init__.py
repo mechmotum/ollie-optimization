f2f3ed515cea7c093a046b499a43c16d73f5c177
Sam Brockie <sambrockie@icloud.com>
2023-04-19 15:05:13 +0200

    Rename ForceHuman controller to PointMassHuman

--- ollie/__init__.py
+++ ollie/__init__.py
@@ -1,6 +1,6 @@
 """Classes and functions in the ``ollie`` package's namespace."""
 
-from ollie.human import ForceHuman
+from ollie.controller import PointMassHuman
 from ollie.ollie import Ollie
 from ollie.material import Glue, Polyurethane, Maple, Steel
 from ollie.skateboard import (
@@ -13,7 +13,7 @@ from ollie.skateboard import (
 )
 
 __all__ = [
-    ForceHuman,
+    PointMassHuman,
     Ollie,
     Glue,
     Polyurethane,
